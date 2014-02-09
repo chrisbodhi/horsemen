bc228b9cd0a59a683bbb9e763c7d9eb458222ab0
chrisbodhi <cboette@gmail.com>
2014-02-09 10:26:21 -0600

    Add more dependencies. Set up database, or at least I hope so.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source "https://rubygems.org"
 
 gem "sinatra"
 gem "haml"
+gem "activerecord"
+gem "sinatra-activerecord"
+gem "pg"
--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'sinatra/activerecord'
+require './config/environments' #db config
 
 get '/' do
   "Hola, mundo."  
